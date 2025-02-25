3bd49777c1e7fb443da9a5e4f563d7b22ef0f00e
Lina Marcela Marin <linamarin2011@gmail.com>
2025-02-25 16:41:48 -0500

    Se agrega graficos de productos

--- admin2/grafica.js
+++ admin2/grafica.js
@@ -52,7 +52,125 @@ const getChartOptions = () => {
     };
 };
 
+const getChartOptions2 = () => {
+    return {
+        series: [5, 10, 20, 5],
+        colors: ["#E81C2E", "#F25757", "#FFB3B3", "#D9D9D9"],  // Paleta de rojos y grises combinada
+        chart: {
+            height: 320,
+            width: "100%",
+            type: "donut",
+        },
+        stroke: {
+            colors: ["transparent"],
+        },
+        plotOptions: {
+            pie: {
+                donut: {
+                    labels: {
+                        show: true,
+                        name: {
+                            show: true,
+                            fontFamily: "Inter, sans-serif",
+                        },
+                        total: {
+                            showAlways: true,
+                            show: true,
+                            label: "Premios",
+                            fontFamily: "Inter, sans-serif",
+                            formatter: function (w) {
+                                const sum = w.globals.seriesTotals.reduce((a, b) => a + b, 0);
+                                return sum;  // Texto total ajustado
+                            },
+                        },
+                        value: {
+                            show: true,
+                            fontFamily: "Inter, sans-serif",
+                            formatter: function (value) {
+                                return value + " servicios";  // Formato de valor ajustado
+                            },
+                        },
+                    },
+                    size: "80%",
+                },
+            },
+        },
+        labels: ["Lavado sencillo", "Jabon", "Brillado", "Polichado"],
+        dataLabels: {
+            enabled: false,
+        },
+        legend: {
+            position: "bottom",
+            fontFamily: "Inter, sans-serif",
+        },
+    };
+};
+
+const getChartOptions3 = () => {
+    return {
+        series: [2, 1, 1, 1],
+        colors: ["#E81C2E", "#F25757", "#FFB3B3", "#D9D9D9"],  // Paleta de rojos y grises combinada
+        chart: {
+            height: 320,
+            width: "100%",
+            type: "donut",
+        },
+        stroke: {
+            colors: ["transparent"],
+        },
+        plotOptions: {
+            pie: {
+                donut: {
+                    labels: {
+                        show: true,
+                        name: {
+                            show: true,
+                            fontFamily: "Inter, sans-serif",
+                        },
+                        total: {
+                            showAlways: true,
+                            show: true,
+                            label: "Premios",
+                            fontFamily: "Inter, sans-serif",
+                            formatter: function (w) {
+                                const sum = w.globals.seriesTotals.reduce((a, b) => a + b, 0);
+                                return sum;  // Texto total ajustado
+                            },
+                        },
+                        value: {
+                            show: true,
+                            fontFamily: "Inter, sans-serif",
+                            formatter: function (value) {
+                                return value + " servicios";  // Formato de valor ajustado
+                            },
+                        },
+                    },
+                    size: "80%",
+                },
+            },
+        },
+        labels: ["Lavado con Aspirado", "Polichado", "Ambientador", "Paño"],
+        dataLabels: {
+            enabled: false,
+        },
+        legend: {
+            position: "bottom",
+            fontFamily: "Inter, sans-serif",
+        },
+    };
+};
+
 if (document.getElementById("donut-chart") && typeof ApexCharts !== 'undefined') {
     const chart = new ApexCharts(document.getElementById("donut-chart"), getChartOptions());
+    chart.render();
+}
+
+if (document.getElementById("donut-chart-2") && typeof ApexCharts !== 'undefined') {
+    const chart = new ApexCharts(document.getElementById("donut-chart-2"), getChartOptions2());
+    chart.render();
+}
+
+if (document.getElementById("donut-chart-3") && typeof ApexCharts !== 'undefined') {
+    const chart = new ApexCharts(document.getElementById("donut-chart-3"), getChartOptions3());
     chart.render();
 }
